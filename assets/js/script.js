3c52a6c98e2674302d2dd7ab8778d18e09d398d2
ElluruBharathSimhaReddy <163523951+ElluruBharathSimhaReddy@users.noreply.github.com>
2025-08-13 19:37:04 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,17 +143,19 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
+    const clickedLink = this;
+    const clickedLinkText = clickedLink.innerHTML.toLowerCase();
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
+    // Remove 'active' class from all links and pages
+    navigationLinks.forEach(link => link.classList.remove("active"));
+    pages.forEach(page => page.classList.remove("active"));
+
+    // Add 'active' class to the clicked link and its corresponding page
+    clickedLink.classList.add("active");
+    const targetPage = document.querySelector(`[data-page="${clickedLinkText}"]`);
+    if (targetPage) {
+      targetPage.classList.add("active");
     }
-
+    window.scrollTo(0, 0);
   });
 }
